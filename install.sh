bdda2cb7a04b3e8995f05e9a37f23742f6cbe9ed
Gordon Burgett <gordon@gordonburgett.net>
2017-10-07 13:38:59 -0600

    Add configurable install location for service

--- install.sh
+++ install.sh
@@ -1,11 +1,55 @@
 #! /bin/bash
 
+COLOR_OFF='\033[0m'       # Text Reset
+COLOR_GREEN='\033[0;32m'
+COLOR_RED='\033[0;31m'
+COLOR_YELLOW='\033[0;33m' 
+COLOR_CYAN='\033[0;36m'
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+BIN_DIR=/bin
+
+usage() { echo -e "`basename $0` ${COLOR_YELLOW}<flags>${COLOR_OFF}
+  Installs sacron and associated dependencies
+
+${COLOR_YELLOW}Flags:${COLOR_OFF}" && grep " .)\ #" $0; }
+
+while getopts ":d:h" opt; do
+  case $opt in
+    h) # print the help
+      usage
+      exit 0
+      ;;
+    d) # The directory to install sacron
+      BIN_DIR=$OPTARG
+      ;;
+    \?)
+      echo -e "${COLOR_RED}Invalid option: -$OPTARG${COLOR_OFF}" >&2
+      usage
+      exit -1
+      ;;
+  esac
+done
+
+[[ ! -d $BIN_DIR ]] && echo -e "${COLOR_RED}Install directory ${BIN_DIR} does not exist" && exit -1
+
+[[ ! -d /etc/init.d ]] && echo -e "${COLOR_RED}It appears that the init.d service does not exist.  Are you running ubuntu?" && exit -1
+
 cp sacron_service /etc/init.d/sacron
 chown root:root /etc/init.d/sacron
-cp sacron.sh /bin/sacron
+cp sacron.sh $BIN_DIR/sacron
 
 cd sunwait-20041208
 make
-cp sunwait /bin/sunwait
+cp sunwait $BIN_DIR/sunwait
+
+# create config
+echo "BIN_DIR=$BIN_DIR" > /etc/sacron.conf
 
 service sacron start
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo "couldn't start as normal service - starting directly"
+  /etc/init.d/sacron start
+fi
--- sacron.sh
+++ sacron.sh
@@ -1,9 +1,26 @@
 #! /bin/bash
 
-
 log() {
   echo "$(date +"%D %T") $@"
 }
+
+error() {
+  local parent_lineno="$1"
+  local message="$2"
+  local code="${3:-1}"
+  if [[ -n "$message" ]] ; then
+    log "Error on or near line ${parent_lineno}: ${message}; exiting with status ${code}"
+  else
+    log "Error on or near line ${parent_lineno}; exiting with status ${code}"
+  fi
+  exit "${code}"
+}
+trap 'error ${LINENO}' ERR
+
+[[ -z "$BIN_DIR" ]] && BIN_DIR=/bin
+
+
+source /etc/sacron.conf || log "no config file /etc/sacron.conf - using defaults"
 
 declare -a scriptures=(
   "Remember the Sabbath day, to keep it holy. Exodus 20:8"
@@ -12,16 +29,25 @@ declare -a scriptures=(
 )
 RANDOM=$$$(date +%s)
 
-# get current latitude and longitude by IP
-IP=`curl -s ipinfo.io/ip`
-LOC=`curl -s ipinfo.io/$IP | grep 'loc' | awk '{ print $2 }'`
+get_lat_lon() {
+  # get current latitude and longitude by IP
+  IP=`curl -s ipinfo.io/ip`
+  LOC=`curl -s ipinfo.io/$IP | grep 'loc' | awk '{ print $2 }'`
 
-# remove quotes and strip
-LOC=$(eval echo $LOC)
+  # remove quotes and strip
+  LOC=$(eval echo $LOC)
 
-IFS=',' read -ra ARR <<< "$LOC"
-LAT=${ARR[0]}
-LON=${ARR[1]}
+  IFS=',' read -ra ARR <<< "$LOC"
+  LAT=${ARR[0]}
+  LON=${ARR[1]}
+}
+
+if [[ -z "$LAT" || -z "$LON" ]]; then
+  get_lat_lon
+  log "Discovered latitude and longitude ${LAT}N, ${LON}E"
+else
+  log "Using configured latitude and longitude ${LAT}N, ${LON}E"
+fi
 
 check_sabbath() {
   if [[ "$TODAY" == "Sat" ]]; then
@@ -31,7 +57,7 @@ check_sabbath() {
 
     # wait for sundown on Sunday
     sleep 30
-    sunwait -v -p sun down ${LAT}N ${LON}E
+    $BIN_DIR/sunwait -v -p sun down ${LAT}N ${LON}E
 
     log "Sabbath is over, time to start cron again"
     service cron start
@@ -43,7 +69,7 @@ do
   TODAY=$(date '+%a')
   log "Today is $TODAY"
 
-  sunwait -v -p sun down ${LAT}N ${LON}E
+  $BIN_DIR/sunwait -v -p sun down ${LAT}N ${LON}E
 
   check_sabbath
 done
